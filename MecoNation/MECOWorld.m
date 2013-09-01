885ded23109bd316a31cc8ba5be17333ba4fd5c7
Micah Merswolke <mecoking@icloud.com>
2013-09-01 16:18:15 -0400

    Implemented Gold, Leaves, and Leather

--- MecoNation/MECOWorld.m
+++ MecoNation/MECOWorld.m
@@ -18,13 +18,16 @@
 -(instancetype)init {
 	if ((self = [super init])) {
 		_foodResource = [MECOResource resourceWithName:@"food"];
+		_leafResource = [MECOResource resourceWithName:@"leaf"];
+		_leatherResource = [MECOResource resourceWithName:@"leather"];
+		_goldResource = [MECOResource resourceWithName:@"gold"];
 		_furResource = [MECOResource resourceWithName:@"fur"];
 		_IQResource = [MECOResource resourceWithName:@"IQ"];
 		_stoneResource = [MECOResource resourceWithName:@"stone"];
 		_woodResource = [MECOResource resourceWithName:@"wood"];
 		_woolResource = [MECOResource resourceWithName:@"wool"];
 		
-		_resources = @[_foodResource, _furResource, _IQResource, _stoneResource, _woodResource, _woolResource];
+		_resources = @[_foodResource, _leafResource, _leatherResource, _goldResource, _furResource, _IQResource, _stoneResource, _woodResource, _woolResource];
 		
 		_jobs = [[MECOJob jobsWithWorld:self] copy];
 		_jobsByTitle = [[NSDictionary dictionaryWithObjects:self.jobs forKeys:[self.jobs valueForKey:@"title"]] copy];
